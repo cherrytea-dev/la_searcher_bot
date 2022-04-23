58fdf4e454fb01c97f5949539beb9806c3a75037
Mike Trifonov <mike.trifonov@gmail.com>
2022-04-23 15:08:56 +0200

    reviving main.py

--- send_notifications/main.py
+++ send_notifications/main.py
@@ -12,12 +12,15 @@ def process_pubsub_message(event):
     """main entry function"""
 
     # receiving message text from pub/sub
-    if 'data' in event:
-        received_message_from_pubsub = base64.b64decode(event['data']).decode('utf-8')
-        encoded_to_ascii = eval(received_message_from_pubsub)
-        data_in_ascii = encoded_to_ascii['data']
-        message_in_ascii = data_in_ascii['message']
-    else:
+    try:
+        if 'data' in event:
+            received_message_from_pubsub = base64.b64decode(event['data']).decode('utf-8')
+            encoded_to_ascii = eval(received_message_from_pubsub)
+            data_in_ascii = encoded_to_ascii['data']
+            message_in_ascii = data_in_ascii['message']
+        else:
+            message_in_ascii = 'ERROR: I cannot read message from pub/sub'
+    except: # noqa
         message_in_ascii = 'ERROR: I cannot read message from pub/sub'
 
     return message_in_ascii
