d6897d1a7418cd35c96eab86f1f1e86e08be441c
Mike Trifonov <mike.trifonov@gmail.com>
2022-04-23 14:36:22 +0200

    reviving main.py

--- send_notifications/main.py
+++ send_notifications/main.py
@@ -1,7 +1,35 @@
+import datetime
+import os
+import base64
+import logging
+
+from telegram import Bot
+
+from google.cloud import secretmanager
+
+
+def process_pubsub_message(event):
+    """main entry function"""
+
+    # receiving message text from pub/sub
+    if 'data' in event:
+        received_message_from_pubsub = base64.b64decode(event['data']).decode('utf-8')
+        encoded_to_ascii = eval(received_message_from_pubsub)
+        data_in_ascii = encoded_to_ascii['data']
+        message_in_ascii = data_in_ascii['message']
+    else:
+        message_in_ascii = 'ERROR: I cannot read message from pub/sub'
+
+    return message_in_ascii
 
 
 def main(event, context): # noqa
 
-    print('hello world')
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    logging.info('script Send_Notifications received from pubsub: {}'.format(pubsub_message))  # noqa
+
+    message_from_pubsub = process_pubsub_message(event)
+
+    logging.info('hello world')
 
     return None
