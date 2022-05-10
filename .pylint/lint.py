cf10b97b792697e0845179efbb69e299a56750df
Mike Trifonov <mike.trifonov@gmail.com>
2022-05-10 19:55:49 +0200

    [che_post]: playing with pylint, try to make an swg, try 17:)

---
+++ .pylint/lint.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+from pylint import lint
+
+THRESHOLD = 9
+
+FILE_NAME = os.getenv('FILE_NAME')
+# example: check_first_posts_for_changes
+
+run = lint.Run([f'../{FILE_NAME}/main.py'], do_exit=False)
+
+score = run.linter.stats["global_note"]
+
+# save the Env Var
+os.environ['LINT_SCORE'] = score
+
+if score == THRESHOLD:
+
+    print("Linter failed: Score < threshold value")
+
+    # exit with error
+    sys.exit(1)
+
+# exit without any errors
+sys.exit(0)
