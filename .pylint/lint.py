9ea3f38d44dd54898eb3bb84f04369d75046d6a4
Mike Trifonov <mike.trifonov@gmail.com>
2022-05-11 22:35:18 +0200

    adding .pylintrc

--- .pylint/lint.py
+++ .pylint/lint.py
@@ -4,23 +4,20 @@ import os
 from pylint import lint
 
 THRESHOLD = 9
+ARGS = ["--rcfile=.pylint/.pylintrc"]
 
 FILE_NAME = os.getenv('FILE_NAME')
 # example: check_first_posts_for_changes
 
-run = lint.Run([f'{FILE_NAME}/main.py'], do_exit=False)
+run = lint.Run([f'{FILE_NAME}/main.py']+ARGS, do_exit=False)
 
 score = str(round(run.linter.stats.global_note, 2))
 
 # save the Env Var
-# os.environ['LINT_SCORE'] = score
-
 env_file = os.getenv('GITHUB_ENV')
 
 with open(env_file, "a") as my_file:
     my_file.write(f"LINT_SCORE={score}")
-
-
 
 if score == THRESHOLD:
 
