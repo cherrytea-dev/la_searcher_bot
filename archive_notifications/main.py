dcbeb9896ae24bca58996562b3f14032a92c1b24
Mike Trifonov <mike.trifonov@gmail.com>
2022-06-24 21:51:25 +0200

    test to upate notif_by_user__archive 3

--- archive_notifications/main.py
+++ archive_notifications/main.py
@@ -176,7 +176,7 @@ def main(event, context):  # noqa
                 logging.info('nothing to migrate in notif_by_user_status')
 
         # TODO: trying to make a copying mechanism notif_by_user_status__history -> notif_by_user__archive
-        for i in range(100):
+        for i in range(1):
             stmt = sqlalchemy.text(f"""
                                     SELECT 
                                     message_id, event, event_timestamp,
