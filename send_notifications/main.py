ba3fa6dfd80903f4645bd150b7a0beb83dbc128a
Mike Trifonov <mike.trifonov@gmail.com>
2022-06-26 20:17:54 +0200

    additional checks of new sql query is good 6

--- send_notifications/main.py
+++ send_notifications/main.py
@@ -361,7 +361,8 @@ def check_for_notifs_to_send(conn):
     print(f'CCC: OLD: {msg_w_o_notif}')
     if msg_w_o_notif and tempo and msg_w_o_notif not in {'None', None} and tempo not in {'None', None}:
         print(f'CCC: comparison: {tempo[:2] + tempo[3:] == msg_w_o_notif[:2] + msg_w_o_notif[3:]}')
-        notify_admin(f'CCC: comparison: {tempo[:2] + tempo[3:] == msg_w_o_notif[:2] + msg_w_o_notif[3:]}')
+        if tempo[:2] + tempo[3:] != msg_w_o_notif[:2] + msg_w_o_notif[3:]:
+            notify_admin(f'CCC: comparison FALSE')
 
     return msg_w_o_notif
 
