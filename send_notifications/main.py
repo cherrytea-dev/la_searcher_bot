176e893f752d7502ea9bdec080a8768c40279f3d
Mike Trifonov <mike.trifonov@gmail.com>
2022-06-26 19:17:31 +0200

    additional checks of new sql query is good 5

--- send_notifications/main.py
+++ send_notifications/main.py
@@ -235,7 +235,8 @@ def write_message_sending_status(conn_, message_id_, result, mailing_id_, change
 
             logging.info(f'[send_notif]: message {message_id_}, sending status is {result} for conn')
 
-            with sql_connect().connect() as conn2:
+            pool = sql_connect()
+            with pool.connect() as conn2:
 
                 conn2.execute(sql_text,
                               a=message_id_,
@@ -247,8 +248,10 @@ def write_message_sending_status(conn_, message_id_, result, mailing_id_, change
                               g=message_type_,
                               h='send_notifs'
                               )
+                conn2.close()
+            pool.dispose()
             # TODO: debug notify
-            notify_admin('Send_notifications: message {}, sending status is {} for conn2'.format(message_id_, result))
+            notify_admin('[send_notif]: conn2: message {}, sending status is {} for conn2'.format(message_id_, result))
 
     except Exception as e:  # noqa
         notify_admin(f'[send_notif]: ERR write to SQL notif_by_user_status, message_id {message_id_}, status {result}')
@@ -357,8 +360,8 @@ def check_for_notifs_to_send(conn):
     # TODO: DELETE
     print(f'CCC: OLD: {msg_w_o_notif}')
     if msg_w_o_notif and tempo and msg_w_o_notif not in {'None', None} and tempo not in {'None', None}:
-        print(f'CCC: comparison: {tempo[:3] + tempo[4:] == msg_w_o_notif[:3] + msg_w_o_notif[4:]}')
-        notify_admin(f'CCC: comparison: {tempo[:3] + tempo[4:] == msg_w_o_notif[:3] + msg_w_o_notif[4:]}')
+        print(f'CCC: comparison: {tempo[:2] + tempo[3:] == msg_w_o_notif[:2] + msg_w_o_notif[3:]}')
+        notify_admin(f'CCC: comparison: {tempo[:2] + tempo[3:] == msg_w_o_notif[:2] + msg_w_o_notif[3:]}')
 
     return msg_w_o_notif
 
