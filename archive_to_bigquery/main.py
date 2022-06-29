b230d0ab15dd82b4d0041779bd770d276db8c9b9
Mike Trifonov <mike.trifonov@gmail.com>
2022-06-29 21:36:48 +0200

    set the steps 1-5, wothout deletion of psql

--- archive_to_bigquery/main.py
+++ archive_to_bigquery/main.py
@@ -1,5 +1,6 @@
 """move data from Cloud SQL to BigQuery for long-term storage & analysis"""
 
+import logging
 from google.cloud import bigquery
 
 
@@ -8,20 +9,74 @@ def main(event, context): # noqa
 
     client = bigquery.Client()
 
+    # 1. Get the initial row count of bq table
     query = """
-    SELECT
-      failed
-    FROM
-      `lizaalert-bot-01.notif_archive.notif_by_user__history_20220626_100000`
-    WHERE
-      failed IS NOT NULL
-    LIMIT
-      2
-    """
+        SELECT
+            count(*)
+        FROM 
+            notif_archive.notif_by_user__archive
+        """
 
-    query_job = client.query(query)
+    query_initial_rows_bq = client.query(query)
+    logging.info(f'initial rows in BQ: {query_initial_rows_bq}')
 
-    for row in query_job:
-        print(str(row))
+    # 2. Get the initial row count of cloud sql table
+    query = """
+            SELECT
+                *
+            FROM
+                EXTERNAL_QUERY("projects/lizaalert-bot-01/locations/europe-west3/connections/bq_to_cloud_sql",
+            "SELECT count(*) FROM notif_by_user__history;")
+            """
+
+    query_initial_rows_psql = client.query(query)
+    logging.info(f'initial rows in cloud sql: {query_initial_rows_psql}')
+
+    # 3. Copy all the new rows from psql to bq
+    query = """
+            INSERT INTO
+                notif_archive.notif_by_user__archive (message_id, mailing_id, user_id, message_content, 
+                message_text, message_type, message_params, message_group_id, change_log_id, created, completed, 
+                cancelled, failed, num_of_fails)
+            SELECT *
+            FROM
+                EXTERNAL_QUERY("projects/lizaalert-bot-01/locations/europe-west3/connections/bq_to_cloud_sql",
+                "SELECT * FROM notif_by_user__history;")
+            WHERE NOT message_id IN (
+                SELECT message_id FROM notif_archive.notif_by_user__archive) 
+                """
+
+    query_move = client.query(query)
+    logging.info(f'move from cloud sql to bq: {query_move}')
+
+    # 4. Validate that there are no doubling message_ids in the final bq table
+    query = """
+        SELECT
+            message_id, count(*)
+        FROM 
+            notif_archive.notif_by_user__archive
+        GROUP BY 1
+        HAVING count(*) > 1
+        """
+
+    query_job_final_check = client.query(query)
+    logging.info(f'final check says: {query_job_final_check}')
+
+    # 5. Get the resulting row count of bq table
+    query = """
+        SELECT
+            count(*)
+        FROM 
+            notif_archive.notif_by_user__archive
+        """
+
+    query_resulting_rows_bq = client.query(query)
+    logging.info(f'resulting rows in BQ: {query_resulting_rows_bq}')
+
+    # 6. Delete data from cloud sql
+    # query = """ """
+
+    # query_delete = client.query(query)
+    # print(f'move from cloud sql to bq: {query_move}')
 
     return None
