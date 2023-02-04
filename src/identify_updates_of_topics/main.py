1c149ee3828a904c6067f0959a73b0b9c7685d8c
Mike Trifonov <mike.trifonov@gmail.com>
2023-02-04 15:39:02 +0100

    try to save city_coords in searches 12

--- src/identify_updates_of_topics/main.py
+++ src/identify_updates_of_topics/main.py
@@ -2823,8 +2823,9 @@ def update_change_log_and_searches(db, folder_num):
                 snapshot_line.topic_type, snapshot_line.display_name, snapshot_line.age_min, \
                 snapshot_line.age_max, snapshot_line.new_status, snapshot_line.locations = list(line)
 
-            notify_admin(f'snp.loc {type(snapshot_line.locations)}, {snapshot_line.locations}')
-            # snapshot_line.locations = snapshot_line.locations
+            notify_admin(f'snp.loc 1 {type(snapshot_line.locations)}, {snapshot_line.locations}')
+            snapshot_line.locations = json.loads(snapshot_line.locations)
+            notify_admin(f'snp.loc 2 {type(snapshot_line.locations)}, {snapshot_line.locations}')
             curr_snapshot_list.append(snapshot_line)
 
         # TODO - in future: should the number of searches be limited? Probably to JOIN change_log and WHERE folder=...
