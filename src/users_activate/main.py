3619c03d3196ac357c49f4bd6cc0eff47b5af63b
Mike Trifonov <mike.trifonov@gmail.com>
2023-06-14 09:56:16 +0200

    set onb step_id=80 for ones with summaries

--- src/users_activate/main.py
+++ src/users_activate/main.py
@@ -437,6 +437,35 @@ def mark_up_onboarding_status_80_wo_dialogs(cur):
     return None
 
 
+def mark_up_onboarding_status_80_just_got_summaries(cur):
+    """marks up Onboarding step_id=80 for existing old users"""
+
+    # add the New User into table users
+    cur.execute("""
+                    select * 
+                    from user_view_80 
+                    WHERE onb_step is NULL and receives_summaries is not null
+                    limit 1;
+                """)
+    user_id_to_update = cur.fetchone()
+
+    if user_id_to_update and isinstance(user_id_to_update, tuple) and len(user_id_to_update) > 0:
+        user_id_to_update = user_id_to_update[0]
+        logging.info(f'User {user_id_to_update}, will be assigned with onboarding pref_id=80')
+
+        # save onboarding start
+        cur.execute("""
+                            INSERT INTO user_onboarding 
+                            (user_id, step_name, step_id, timestamp) 
+                            VALUES (%s, 'finished', 80, '2023-05-14 12:39:00.000000')
+                            ;""",
+                    (user_id_to_update,))
+
+    else:
+        logging.info(f'There are no users to assign onboarding pref_id=80.')
+
+    return None
+
 
 def main(event, context): # noqa
     """main function"""
@@ -458,9 +487,11 @@ def main(event, context): # noqa
         # mark_up_onboarding_status_80(cur)
         # mark_up_onboarding_status_80_patch(cur)
         # mark_up_onboarding_status_80_wo_dialogs(cur)
+
         for i in range(5):
-            mark_up_onboarding_status_0_2(cur)
-            mark_up_onboarding_status_10_2(cur)
+            # mark_up_onboarding_status_0_2(cur)
+            # mark_up_onboarding_status_10_2(cur)
+            mark_up_onboarding_status_80_just_got_summaries(cur)
 
     except Exception as e:
         logging.error('User activation script failed')
