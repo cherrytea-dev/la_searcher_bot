8826d23f5afec0f86b4898f4359318fef9417a50
Mike Trifonov <mike.trifonov@gmail.com>
2024-02-07 19:09:17 +0100

    [send_notifs] experiment with inline reply markup 0

--- src/send_notifications/main.py
+++ src/send_notifications/main.py
@@ -134,6 +134,22 @@ def send_message_to_api(session, bot_token, user_id, message, params):
                 parse_mode = f'&parse_mode={params["parse_mode"]}'
             if 'disable_web_page_preview' in params.keys():
                 disable_web_page_preview = f'&disable_web_page_preview={params["disable_web_page_preview"]}'
+            if 'reply_markup' in params.keys():
+                # FIXME – 07.02.24 – experimenting
+                reply_markup_temp = params['reply_markup']
+                logging.info(f'{reply_markup_temp=}')
+                reply_markup_json = json.dumps(reply_markup_temp)
+                logging.info(f'{reply_markup_json=}')
+                reply_markup_string = str(reply_markup_json)
+                logging.info(f'{reply_markup_string=}')
+                reply_markup = f'&reply_markup={{{reply_markup_string}}}'
+                logging.info(f'{reply_markup=}')
+                reply_markup_encoded = urllib.parse.quote(reply_markup)
+                logging.info(f'{reply_markup_encoded=}')
+                if user_id == 438843471:
+                    notify_admin(f'{reply_markup=}')
+                # FIXME ^^^
+
         message_encoded = urllib.parse.quote(message)
 
         request_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={user_id}' \
