a872f30b14b0b167dbac5fee57440413e1241efe
Mike Trifonov <mike.trifonov@gmail.com>
2024-10-27 17:08:43 +0100

    improving linter 10

--- src/api_get_active_searches/main.py
+++ src/api_get_active_searches/main.py
@@ -390,7 +390,7 @@ def main(request):
     # Set CORS headers for the preflight request
     if request.method == "OPTIONS":
         # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
+        # header and caches preflight response for 3600s
         headers = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": ["GET", "OPTIONS"],
