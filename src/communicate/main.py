fa21e4091bd67778c6f8da1699c0371bed5eaf08
AKagitin <125404370+AKagitin@users.noreply.github.com>
2025-01-18 13:03:20 +0300

    Fix Object of type ReplyKeyboardMarkup is not JSON serializable

--- src/communicate/main.py
+++ src/communicate/main.py
@@ -2168,6 +2168,7 @@ def make_api_call(method: str, bot_api_token: str, params: dict, call_context=''
 
     url = f'https://api.telegram.org/bot{bot_api_token}/{method}'  # e.g. sendMessage
     headers = {'Content-Type': 'application/json'}
+    params['reply_markup'] = params['reply_markup'].to_dict()
     logging.info(f'({method=}, {call_context=})..before json_params = json.dumps(params) {params=}; {type(params)=}')
     json_params = json.dumps(params)
     logging.info(f'({method=}, {call_context=})..after json.dumps(params): {json_params=}; {type(json_params)=}')
