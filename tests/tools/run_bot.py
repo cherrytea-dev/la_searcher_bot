93fb29ba31c3bd32e28e3c5f72abf2d58f1fd9f7
volodkindv <volodkindv@skbkontur.ru>
2025-01-18 20:13:16 +0500

    fix bot env; move start bot script to tests

--- tests/tools/run_bot.py
+++ tests/tools/run_bot.py
@@ -1,19 +1,21 @@
 import asyncio
+from functools import lru_cache
 from unittest.mock import patch
 
 import nest_asyncio
+from dotenv import load_dotenv
 from pyannotate_runtime import collect_types
 from telegram import Bot
 from telegram.ext import Updater
 
+from _dependencies.commons import AppConfig
 from communicate.main import process_update
-from tests.common import get_test_config
 
 nest_asyncio.apply()
 
 
 async def main_bot() -> None:
-    bot = Bot(get_test_config().bot_api_token__prod)
+    bot = Bot(get_dotenv_config().bot_api_token__prod)
     update_queue = asyncio.Queue()
     updater = Updater(bot, update_queue)
     async with updater:
@@ -24,9 +26,15 @@ async def main_bot() -> None:
             process_update(update)
 
 
+@lru_cache
+def get_dotenv_config() -> AppConfig:
+    assert load_dotenv('.env', override=True)
+    return AppConfig()
+
+
 if __name__ == '__main__':
     with (
-        patch('_dependencies.commons._get_config', get_test_config),
+        patch('_dependencies.commons._get_config', get_dotenv_config),
         patch('_dependencies.commons.get_publisher'),
         patch('_dependencies.commons.get_project_id'),
     ):
@@ -35,6 +43,6 @@ if __name__ == '__main__':
         with collect_types.collect():
             try:
                 asyncio.run(main_bot())
-            except:
+            except Exception as exc:
                 pass  # let the pyannotate to save collected types
         collect_types.dump_stats('type_info.json')
