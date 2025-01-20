85a8b540940717e4414e3d19b999dbc7e84c798d
volodkindv <volodkindv@skbkontur.ru>
2025-01-20 23:33:05 +0500

    test_connect_to_forum

--- tests/test_run_bot.py
+++ tests/test_run_bot.py
@@ -2,25 +2,15 @@ import asyncio
 
 import nest_asyncio
 
-from communicate.main import process_leaving_chat_async, process_sending_message_async
+from _dependencies.misc import process_sending_message_async
 
 nest_asyncio.apply()
-# from asgiref.sync import async_to_sync, sync_to_async
 
 
-def test_1():
+def test_nested_async():
     asyncio.run(inner())
 
 
-def test_2():
-    asyncio.run(inner_2())
-
-
 async def inner():
-    process_leaving_chat_async(2)
-    pass
-
-
-async def inner_2():
     process_sending_message_async(2, '')
     pass
