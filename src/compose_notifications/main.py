c17f14287dea5991c8d26e20d2f0ef7eb8c15784
AKagitin <125404370+AKagitin@users.noreply.github.com>
2025-01-26 10:28:21 +0300

    issue#591 separate phone numbers from comment url

--- src/compose_notifications/main.py
+++ src/compose_notifications/main.py
@@ -715,9 +715,15 @@ def compose_com_msg_on_new_comments(line: LineInChangeLog):
     for comment in line.comments:
         if comment.text:
             comment_text = f'{comment.text[:500]}...' if len(comment.text) > 500 else comment.text
+
+            comment_text = add_tel_link(comment_text)
+            code_pos = text.find('<code>')
+            text_before_code_pos = text[:code_pos]
+            text_from_code_pos = text[code_pos:]
+
             msg += (
                 f' &#8226; <a href="{url_prefix}{comment.author_link}">{comment.author_nickname}</a>: '
-                f'<i>«<a href="{comment.url}">{comment_text}</a>»</i>\n'
+                f'<i>«<a href="{comment.url}">{text_before_code_pos}</a>{text_from_code_pos}»</i>\n'
             )
 
     msg = f'Новые комментарии по {activity} {line.clickable_name}:\n{msg}' if msg else ''
