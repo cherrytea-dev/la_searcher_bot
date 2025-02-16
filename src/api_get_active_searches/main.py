84ee02eebb7dc402bb12632b2df942239bbe09a1
volodkindv <volodkindv@skbkontur.ru>
2025-02-16 14:36:31 +0500

    refact api_get_active_searches: add tests; add mypy; fix failed tests.

--- src/api_get_active_searches/main.py
+++ src/api_get_active_searches/main.py
@@ -1,14 +1,16 @@
 """Function acts as API for the App designed to support LizaAlert Group of Phone Calls.
 The current script retrieves an actual list active searches"""
 
+import ast
+import datetime
 import json
 import logging
-import re
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any
 
 import functions_framework
-from bs4 import BeautifulSoup
 from flask import Request
+from psycopg2.extensions import connection
+from pydantic import BaseModel, ConfigDict, Field, ValidationError
 
 from _dependencies.commons import get_app_config, setup_google_logging, sql_connect_by_psycopg2
 from _dependencies.content import clean_up_content
@@ -16,209 +18,49 @@ from _dependencies.content import clean_up_content
 setup_google_logging()
 
 
-def get_list_of_allowed_apps():
-    """get the list of app_ids approved by admin"""
-
-    approved_app_ids = None
-
-    try:
-        data_string = get_app_config().api_clients
-        approved_app_ids = eval(data_string)
-
-    except Exception as e:
-        logging.exception(e)
-        logging.info('exception happened in getting list of allowed app_ids')
-
-    return approved_app_ids
+class Search(BaseModel):
+    search_start_time: datetime.datetime
+    forum_folder_id: int
+    search_type: str
+    search_id: int
+    search_status: str
+    display_name: str
+    family_name: str
+    age_min: int
+    age_max: int
+    content: str
 
 
-def get_list_of_active_searches_from_db(request: json) -> tuple:
-    """retrieves a list of recent searches"""
-
-    depth_days = 10000
-    if 'depth_days' in request.keys() and isinstance(request['depth_days'], int):
-        depth_days = request['depth_days']
-    logging.info(f'{depth_days=}')
-
-    folders_list = []
-    if 'forum_folder_id_list' in request.keys() and isinstance(request['forum_folder_id_list'], list):
-        folders_list = request['forum_folder_id_list']
-    logging.info(f'{folders_list=}')
-
-    searches_data = []
-    conn_psy = sql_connect_by_psycopg2()
-    cur = conn_psy.cursor()
-
-    if folders_list:
-        cur.execute(
-            """WITH
-            user_regions_filtered AS (
-                SELECT DISTINCT folder_id AS forum_folder_num
-                FROM geo_folders
-                WHERE folder_type='searches' AND folder_id = ANY(%s)
-            ),
-            s2 AS (
-                SELECT search_start_time, forum_folder_id, topic_type, search_forum_num,
-                        status, display_name, family_name,
-                age_min, age_max
-                FROM searches
-                WHERE forum_folder_id IN (SELECT forum_folder_num FROM user_regions_filtered)
-                AND status NOT IN ('НЖ', 'НП', 'Завершен', 'Найден')
-                AND topic_type_id != 1
-                AND search_start_time >= CURRENT_TIMESTAMP - INTERVAL '%s days'
-                ORDER BY search_start_time DESC
-            ),
-            s3 AS (SELECT s2.*
-                FROM s2
-                LEFT JOIN search_health_check shc
-                ON s2.search_forum_num=shc.search_forum_num
-                WHERE (shc.status is NULL OR shc.status='ok' OR shc.status='regular')
-                ORDER BY s2.search_start_time DESC
-            ),
-            s4 AS (SELECT s3.*, sfp.content
-                FROM s3
-                LEFT JOIN search_first_posts AS sfp
-                ON s3.search_forum_num=sfp.search_id
-                WHERE sfp.actual = True
-            )
-            SELECT * FROM s4;""",
-            (folders_list, depth_days),
-        )
-    else:
-        cur.execute(
-            """WITH
-            user_regions_filtered AS (
-                SELECT DISTINCT folder_id AS forum_folder_num
-                FROM geo_folders
-                WHERE folder_type='searches'
-            ),
-            s2 AS (
-                SELECT search_start_time, forum_folder_id, topic_type, search_forum_num,
-                        status, display_name, family_name,
-                age_min, age_max
-                FROM searches
-                WHERE forum_folder_id IN (SELECT forum_folder_num FROM user_regions_filtered)
-                AND status NOT IN ('НЖ', 'НП', 'Завершен', 'Найден')
-                AND topic_type_id != 1
-                AND search_start_time >= CURRENT_TIMESTAMP - INTERVAL '%s days'
-                ORDER BY search_start_time DESC
-            ),
-            s3 AS (SELECT s2.*
-                FROM s2
-                LEFT JOIN search_health_check shc
-                ON s2.search_forum_num=shc.search_forum_num
-                WHERE (shc.status is NULL OR shc.status='ok' OR shc.status='regular')
-                ORDER BY s2.search_start_time DESC
-            ),
-            s4 AS (SELECT s3.*, sfp.content
-                FROM s3
-                LEFT JOIN search_first_posts AS sfp
-                ON s3.search_forum_num=sfp.search_id
-                WHERE sfp.actual = True
-            )
-            SELECT * FROM s4;""",
-            (depth_days,),
-        )
-
-    raw_data = cur.fetchall()
-
-    if raw_data:
-        for line in raw_data:
-            (
-                search_start_time,
-                forum_folder_id,
-                topic_type,
-                search_id,
-                status,
-                display_name,
-                family_name,
-                age_min,
-                age_max,
-                first_post,
-            ) = line
-
-            # search_id, search_start_time, display_name, status, family_name, topic_type, topic_type_id, \
-            # city_locations, age_min, age_max, first_post, lat, lon, coord_type, last_change_time = line
-
-            logging.info(f'{search_id=}')
-
-            # define "content"
-            content = clean_up_content(first_post)
-
-            user_search = {
-                'search_start_time': search_start_time,
-                'forum_folder_id': forum_folder_id,
-                'search_type': topic_type,
-                'search_id': search_id,
-                'search_status': status,
-                'display_name': display_name,
-                'family_name': family_name,
-                'age_min': age_min,
-                'age_max': age_max,
-                'content': content,
-            }
-
-            searches_data.append(user_search)
-
-    cur.close()
-    conn_psy.close()
-
-    return searches_data
+class UserRequest(BaseModel):
+    model_config = ConfigDict(extra='ignore')
+    app_id: int | str
+    forum_folder_id_list: list[int] = Field(default_factory=list)
+    depth_days: int = 10000
 
 
-def save_user_statistics_to_db(user_input, response) -> None:
-    """save user's interaction into DB"""
-
-    json_to_save = json.dumps(response, default=str)
-
-    conn_psy = sql_connect_by_psycopg2()
-    cur = conn_psy.cursor()
-
-    try:
-        cur.execute(
-            """INSERT INTO stat_api_usage_actual_searches
-                       (request, timestamp, response)
-                       VALUES (%s, CURRENT_TIMESTAMP, %s);""",
-            (str(user_input), json_to_save),
-        )
-
-    except Exception as e:
-        logging.exception(e)
-
-    cur.close()
-    conn_psy.close()
-
-    return None
+class FlaskResponseBase(BaseModel):
+    def as_response(self) -> tuple[str, int, dict]:
+        headers = {'Access-Control-Allow-Origin': '*'}
+        return self.model_dump_json(), 200, headers
 
 
-def verify_json_validity(user_input, list_of_allowed_apps):
-    """verify the received message is eligible to be processed"""
-
-    reason = None
-
-    if not user_input or not isinstance(user_input, dict):  # or 'hash' not in user_input.keys():
-        reason = 'No request or request is not a dict/json'
-
-    elif 'app_id' not in user_input.keys():
-        reason = 'No app_id provided'
-
-    elif user_input['app_id'] not in list_of_allowed_apps:
-        reason = 'Incorrect app_id'
-
-    logging.info(f'the incoming json is {user_input=}, {reason=}')
-
-    return reason
+class FailResponse(FlaskResponseBase):
+    reason: str
+    ok: bool = False
 
 
-@functions_framework.http
-def main(request: Request) -> Tuple[str, int, Dict[str, str]]:
-    # For more information about CORS and CORS preflight requests, see:
-    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+class SuccessfulResponse(FlaskResponseBase):
+    searches: list[Search]
+    ok: bool = True
 
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
+
+class OptionsResponse(FlaskResponseBase):
+    def as_response(self) -> tuple[str, int, dict]:
         # Allows GET requests from any origin with the Content-Type
         # header and caches preflight response for 3600s
+        # For more information about CORS and CORS preflight requests, see:
+        # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': ['GET', 'OPTIONS'],
@@ -230,29 +72,141 @@ def main(request: Request) -> Tuple[str, int, Dict[str, str]]:
 
         return '', 204, headers
 
-    # Set CORS headers for the main request
-    headers = {'Access-Control-Allow-Origin': '*'}
+
+def get_list_of_allowed_apps() -> list[str]:
+    """get the list of app_ids approved by admin"""
+
+    approved_app_ids = []
+
+    try:
+        data_string = get_app_config().api_clients
+        approved_app_ids = ast.literal_eval(data_string)
+
+    except Exception as e:
+        logging.exception('exception happened in getting list of allowed app_ids')
+        # but we cannot check app_id later
+
+    return approved_app_ids
+
+
+def get_list_of_active_searches_from_db(conn_psy: connection, request: UserRequest) -> list[Search]:
+    """retrieves a list of recent searches"""
+
+    searches = get_query_results(conn_psy, request.depth_days, request.forum_folder_id_list)
+
+    for line in searches:
+        line.content = str(clean_up_content(line.content))
+
+    return searches
+
+
+def get_query_results(conn_psy: connection, depth_days: int, folders_list: list[int]) -> list[Search]:
+    with conn_psy.cursor() as cur:
+        query = f"""
+            WITH
+                user_regions_filtered AS (
+                    SELECT DISTINCT folder_id AS forum_folder_num
+                    FROM geo_folders
+                    WHERE folder_type='searches' 
+                    {'AND folder_id = ANY(%s)' if folders_list else ''}
+                ),
+                s2 AS (
+                    SELECT search_start_time, forum_folder_id, topic_type, search_forum_num,
+                            status, display_name, family_name,
+                    age_min, age_max
+                    FROM searches
+                    WHERE forum_folder_id IN (SELECT forum_folder_num FROM user_regions_filtered)
+                    AND status NOT IN ('НЖ', 'НП', 'Завершен', 'Найден')
+                    AND topic_type_id != 1
+                    AND search_start_time >= CURRENT_TIMESTAMP - INTERVAL '%s days'
+                    ORDER BY search_start_time DESC
+                ),
+                s3 AS (SELECT s2.*
+                    FROM s2
+                    LEFT JOIN search_health_check shc
+                    ON s2.search_forum_num=shc.search_forum_num
+                    WHERE (shc.status is NULL OR shc.status='ok' OR shc.status='regular')
+                    ORDER BY s2.search_start_time DESC
+                ),
+                s4 AS (SELECT s3.*, sfp.content
+                    FROM s3
+                    LEFT JOIN search_first_posts AS sfp
+                    ON s3.search_forum_num=sfp.search_id
+                    WHERE sfp.actual = True
+                )
+                SELECT * FROM s4;
+                """
+
+        args = (folders_list, depth_days) if folders_list else (depth_days,)
+        cur.execute(query, args)
+
+        raw_data = cur.fetchall()
+
+    searches = [
+        Search(
+            search_start_time=line[0],
+            forum_folder_id=line[1],
+            search_type=line[2],
+            search_id=line[3],
+            search_status=line[4],
+            display_name=line[5],
+            family_name=line[6],
+            age_min=line[7],
+            age_max=line[8],
+            content=line[9],
+        )
+        for line in raw_data
+    ]
+    return searches
+
+
+def save_user_statistics_to_db(conn_psy: connection, user_input: Any, response: dict[str, Any]) -> None:
+    """save user's interaction into DB"""
+
+    # TODO accept connection in args
+    json_to_save = json.dumps(response, default=str)
+
+    with conn_psy.cursor() as cur:
+        try:
+            cur.execute(
+                """INSERT INTO stat_api_usage_actual_searches
+                        (request, timestamp, response)
+                        VALUES (%s, CURRENT_TIMESTAMP, %s);""",
+                (str(user_input), json_to_save),
+            )
+
+        except Exception as e:
+            logging.exception('Cannot save statistics to DB')
+
+
+@functions_framework.http
+def main(request: Request) -> tuple[str, int, dict[str, str]]:
+    # Set CORS headers for the preflight request
+    response: FlaskResponseBase
+
+    if request.method == 'OPTIONS':
+        return OptionsResponse().as_response()
 
     request_json = request.get_json(silent=True)
-    logging.info(f'{request_json}')
-    list_of_allowed_apps = get_list_of_allowed_apps()
-    logging.info(f'{list_of_allowed_apps=}')
-    reason_not_to_process_json = verify_json_validity(request_json, list_of_allowed_apps)
+    logging.info(request_json)
 
-    if reason_not_to_process_json:
-        response = {'ok': False, 'reason': reason_not_to_process_json}
+    with sql_connect_by_psycopg2() as conn_psy:
+        try:
+            user_request = UserRequest.model_validate_json(request.data)
+        except ValidationError as ve:
+            response = FailResponse(reason=str(ve))
+            save_user_statistics_to_db(conn_psy, request_json, response.model_dump())
+            return response.as_response()
 
-        save_user_statistics_to_db(request_json, response)
+        if user_request.app_id not in get_list_of_allowed_apps():
+            return FailResponse(reason='Incorrect app_id').as_response()
 
-        return json.dumps(response), 200, headers
+        searches = get_list_of_active_searches_from_db(conn_psy, user_request)
+        response = SuccessfulResponse(searches=searches)
 
-    searches = get_list_of_active_searches_from_db(request_json)
-    response = {'ok': True, 'searches': searches}
-
-    save_user_statistics_to_db(request_json, response)
+        save_user_statistics_to_db(conn_psy, request_json, response.model_dump())
 
     logging.info(request)
-    logging.info(request_json)
     logging.info(f'the RESULT {response}')
 
-    return json.dumps(response, default=str), 200, headers
+    return response.as_response()
--- tests/smoke/test_api_get_active_searches_generated.py
+++ tests/smoke/test_api_get_active_searches_generated.py
@@ -4,11 +4,6 @@ from api_get_active_searches import main
 from tests.common import run_smoke
 
 
-def test_get_list_of_active_searches_from_db():
-    res = run_smoke(main.get_list_of_active_searches_from_db)
-    pass
-
-
 def test_get_list_of_allowed_apps():
     res = run_smoke(main.get_list_of_allowed_apps)
     pass
@@ -21,9 +16,4 @@ def test_main():
 
 def test_save_user_statistics_to_db():
     res = run_smoke(main.save_user_statistics_to_db)
-    pass
-
-
-def test_verify_json_validity():
-    res = run_smoke(main.verify_json_validity)
     pass
