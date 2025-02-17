b8c4d65168b1e8ce37237b8a9ce34bd62a8de82e
AKagitin <125404370+AKagitin@users.noreply.github.com>
2025-02-17 09:31:07 +0300

    def test_topic_new_search for a href tel

--- src/compose_notifications/_utils/commons.py
+++ src/compose_notifications/_utils/commons.py
@@ -112,7 +112,7 @@ def add_tel_link(incoming_text: str) -> str:
     for num in nums:
         try:
             outcome_text = outcome_text.replace(num, ' <a href="tel:' + str(num) + '">' + str(num) + '</a> ')
-        except Exception as e:  # noqa
+        except Exception as e: # noqa
             logging.exception(e)
             outcome_text = outcome_text.replace(num, '<code>' + str(num) + '</code>') # previous version
 
