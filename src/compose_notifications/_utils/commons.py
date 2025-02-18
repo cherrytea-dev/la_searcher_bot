235bd7218fd126ceec48cb6fa4400f0613438726
AKagitin <125404370+AKagitin@users.noreply.github.com>
2025-02-18 10:16:17 +0000

    make lint

--- src/compose_notifications/_utils/commons.py
+++ src/compose_notifications/_utils/commons.py
@@ -112,11 +112,11 @@ def add_tel_link(incoming_text: str) -> str:
     for num in nums:
         try:
             outcome_text = outcome_text.replace(num, ' <a href="tel:' + str(num) + '">' + str(num) + '</a> ')
-        except Exception as e: # noqa (1 space before comment)
+        except Exception as e:  # noqa (1 space before comment)
             logging.exception(e)
-            outcome_text = outcome_text.replace(num, '<code>' + str(num) + '</code>') # previous version (1 space before comment)
-
-        
+            outcome_text = outcome_text.replace(
+                num, '<code>' + str(num) + '</code>'
+            )  # previous version (1 space before comment)
 
     phpbb_tags_to_delete = {'[tel]', '[/tel]'}
     for tag in phpbb_tags_to_delete:
