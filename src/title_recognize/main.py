37c8e054a927bca13ac82c63d8a16826e7fbd10c
volodkindv <volodkindv@skbkontur.ru>
2025-03-09 12:59:57 +0500

    add logging to title_recognize

--- src/title_recognize/main.py
+++ src/title_recognize/main.py
@@ -1,8 +1,9 @@
+import logging
 from typing import Any
 
 import functions_framework
 from flask import Request
-from pydantic import BaseModel, ConfigDict, Field, ValidationError
+from pydantic import BaseModel, ConfigDict, ValidationError
 
 from _dependencies.commons import setup_google_logging
 
@@ -42,12 +43,16 @@ def main(request: Request) -> str:
 
     try:
         user_request = UserRequest.model_validate_json(request.data)
+        logging.info(f'Received request data: {user_request}')
     except ValidationError as ve:
+        logging.info(f'Incorrect request data: {request.data.decode()}')
         return FailResponse(fail_reason=str(ve)).as_response()
 
     reco_title = recognize_title(user_request.title, user_request.reco_type)
 
     if not reco_title or ('topic_type' in reco_title.keys() and reco_title['topic_type'] == 'UNRECOGNIZED'):
         return FailResponse(fail_reason='not able to recognize', request=str(request.data)).as_response()
+
+    logging.info(f'Response: {reco_title}')
 
     return OkResponse(title=user_request.title, recognition=reco_title).as_response()
