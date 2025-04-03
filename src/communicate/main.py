5173969234537f0aec1ffd3f07c51fd5f9ab13ac
volodkindv <volodkindv@skbkontur.ru>
2025-04-04 01:26:21 +0500

    buttons pt2

--- src/communicate/main.py
+++ src/communicate/main.py
@@ -5,14 +5,13 @@
 import datetime
 import logging
 import re
-from typing import Any, Tuple, Union
+from typing import Tuple, Union
 
 import requests
 from flask import Request
 from telegram import (
     Bot,
     InlineKeyboardMarkup,
-    KeyboardButton,
     ReplyKeyboardMarkup,
     ReplyKeyboardRemove,
     Update,
@@ -27,6 +26,79 @@ from _dependencies.commons import (
 from _dependencies.misc import (
     notify_admin,
     process_sending_message_async,
+)
+from communicate._utils.buttons import (
+    b_act_all,
+    b_act_all_comments,
+    b_act_all_in_followed_search,
+    b_act_coords_change,
+    b_act_field_trips_change,
+    b_act_field_trips_new,
+    b_act_first_post_change,
+    b_act_inforg_com,
+    b_act_new_search,
+    b_act_stat_change,
+    b_act_titles,
+    b_back_to_start,
+    b_coords_auto_def,
+    b_coords_check,
+    b_coords_del,
+    b_coords_man_def,
+    b_deact_all,
+    b_deact_all_comments,
+    b_deact_all_in_followed_search,
+    b_deact_coords_change,
+    b_deact_field_trips_change,
+    b_deact_field_trips_new,
+    b_deact_first_post_change,
+    b_deact_inforg_com,
+    b_deact_new_search,
+    b_deact_stat_change,
+    b_fed_dist_pick_other,
+    b_goto_community,
+    b_goto_first_search,
+    b_goto_photos,
+    b_map,
+    b_menu_set_region,
+    b_no_its_not_me,
+    b_orders_done,
+    b_orders_tbd,
+    b_other,
+    b_pref_urgency_high,
+    b_pref_urgency_highest,
+    b_pref_urgency_low,
+    b_pref_urgency_medium,
+    b_reg_moscow,
+    b_reg_not_moscow,
+    b_role_iam_la,
+    b_role_looking_for_person,
+    b_role_other,
+    b_role_secret,
+    b_role_want_to_be_la,
+    b_set_forum_nick,
+    b_set_pref_age,
+    b_set_pref_coords,
+    b_set_pref_notif_type,
+    b_set_pref_radius,
+    b_set_pref_urgency,
+    b_set_topic_type,
+    b_settings,
+    b_view_act_searches,
+    b_view_latest_searches,
+    b_yes_its_me,
+    c_map,
+    c_other,
+    c_settings,
+    c_start,
+    c_view_act_searches,
+    c_view_latest_searches,
+    dict_of_fed_dist,
+    fed_okr_dict,
+    folder_dict,
+    full_dict_of_regions,
+    keyboard_fed_dist_set,
+    keyboard_other,
+    reply_markup_main,
 )
 from communicate._utils.common import AllButtons, save_onboarding_step
 from communicate._utils.compose_messages import (
@@ -52,7 +124,6 @@ from communicate._utils.message_sending import (
     process_leaving_chat_async,
     process_response_of_api_call,
 )
-from communicate._utils.buttons import fed_okr_dict, folder_dict
 
 setup_google_logging()
 
@@ -565,356 +636,6 @@ def process_update(update: Update) -> str:
         return 'finished successfully. it was a system message on bot block/unblock'
 
     b = AllButtons(full_buttons_dict)
-
-    # Buttons & Keyboards
-    # Start & Main menu
-    c_start = '/start'
-    c_view_act_searches = '/view_act_searches'
-    c_view_latest_searches = '/view_latest_searches'
-    c_settings = '/settings'
-    c_other = '/other'
-    c_map = '/map'
-
-    b_role_iam_la = 'я состою в ЛизаАлерт'
-    b_role_want_to_be_la = 'я хочу помогать ЛизаАлерт'
-    b_role_looking_for_person = 'я ищу человека'
-    b_role_other = 'у меня другая задача'
-    b_role_secret = 'не хочу говорить'
-
-    b_orders_done = 'да, заявки поданы'
-    b_orders_tbd = 'нет, но я хочу продолжить'
-
-    # TODO - WIP: FORUM
-    b_forum_check_nickname = 'указать свой nickname с форума'  # noqa
-    b_forum_dont_have = 'у меня нет аккаунта на форуме ЛА'  # noqa
-    b_forum_dont_want = 'пропустить / не хочу говорить'  # noqa
-    # TODO ^^^
-
-    b_pref_urgency_highest = 'самым первым (<2 минуты)'
-    b_pref_urgency_high = 'пораньше (<5 минут)'
-    b_pref_urgency_medium = 'могу ждать (<10 минут)'
-    b_pref_urgency_low = 'не сильно важно (>10 минут)'
-
-    b_yes_its_me = 'да, это я'
-    b_no_its_not_me = 'нет, это не я'
-
-    b_view_act_searches = 'посмотреть актуальные поиски'
-    b_settings = 'настроить бот'
-    b_other = 'другие возможности'
-    b_map = '🔥Карта Поисков 🔥'
-    keyboard_main = [[b_map], [b_view_act_searches], [b_settings], [b_other]]
-    reply_markup_main = ReplyKeyboardMarkup(keyboard_main, resize_keyboard=True)
-
-    # Settings menu
-    b_set_pref_notif_type = 'настроить виды уведомлений'
-    b_set_pref_coords = 'настроить "домашние координаты"'
-    b_set_pref_radius = 'настроить максимальный радиус'
-    b_set_pref_age = 'настроить возрастные группы БВП'
-    b_set_pref_urgency = 'настроить скорость уведомлений'  # <-- TODO: likely to be removed as redundant
-    b_set_pref_role = 'настроить вашу роль'  # <-- TODO # noqa
-    b_set_forum_nick = 'связать аккаунты бота и форума'
-    b_change_forum_nick = 'изменить аккаунт форума'  # noqa
-    b_set_topic_type = 'настроить вид поисков'
-
-    b_back_to_start = 'в начало'
-
-    # Settings - notifications
-    b_act_all = 'включить: все уведомления'
-    b_act_new_search = 'включить: о новых поисках'
-    b_act_stat_change = 'включить: об изменениях статусов'
-    b_act_all_comments = 'включить: о всех новых комментариях'
-    b_act_inforg_com = 'включить: о комментариях Инфорга'
-    b_act_field_trips_new = 'включить: о новых выездах'
-    b_act_field_trips_change = 'включить: об изменениях в выездах'
-    b_act_coords_change = 'включить: о смене места штаба'
-    b_act_first_post_change = 'включить: об изменениях в первом посте'
-    b_act_all_in_followed_search = 'включить: в отслеживаемых поисках - все уведомления'
-    b_deact_all = 'настроить более гибко'
-    b_deact_new_search = 'отключить: о новых поисках'
-    b_deact_stat_change = 'отключить: об изменениях статусов'
-    b_deact_all_comments = 'отключить: о всех новых комментариях'
-    b_deact_inforg_com = 'отключить: о комментариях Инфорга'
-    b_deact_field_trips_new = 'отключить: о новых выездах'
-    b_deact_field_trips_change = 'отключить: об изменениях в выездах'
-    b_deact_coords_change = 'отключить: о смене места штаба'
-    b_deact_first_post_change = 'отключить: об изменениях в первом посте'
-    b_deact_all_in_followed_search = 'отключить: в отслеживаемых поисках - все уведомления'
-
-    # Settings - coordinates
-    b_coords_auto_def = KeyboardButton(text='автоматически определить "домашние координаты"', request_location=True)
-    b_coords_man_def = 'ввести "домашние координаты" вручную'
-    b_coords_check = 'посмотреть сохраненные "домашние координаты"'
-    b_coords_del = 'удалить "домашние координаты"'
-
-    # Dialogue if Region – is Moscow
-    b_reg_moscow = 'да, Москва – мой регион'
-    b_reg_not_moscow = 'нет, я из другого региона'
-
-    # Settings - Federal Districts
-    b_fed_dist_dal_vos = 'Дальневосточный ФО'
-    b_fed_dist_privolz = 'Приволжский ФО'
-    b_fed_dist_sev_kaz = 'Северо-Кавказский ФО'
-    b_fed_dist_sev_zap = 'Северо-Западный ФО'
-    b_fed_dist_sibiria = 'Сибирский ФО'
-    b_fed_dist_uralsky = 'Уральский ФО'
-    b_fed_dist_central = 'Центральный ФО'
-    b_fed_dist_yuzhniy = 'Южный ФО'
-    b_fed_dist_other_r = 'Прочие поиски по РФ'
-    b_fed_dist_pick_other = 'выбрать другой Федеральный Округ'
-    keyboard_fed_dist_set = [
-        [b_fed_dist_dal_vos],
-        [b_fed_dist_privolz],
-        [b_fed_dist_sev_kaz],
-        [b_fed_dist_sev_zap],
-        [b_fed_dist_sibiria],
-        [b_fed_dist_uralsky],
-        [b_fed_dist_central],
-        [b_fed_dist_yuzhniy],
-        [b_fed_dist_other_r],
-        [b_back_to_start],
-    ]
-
-    # Settings - Dalnevostochniy Fed Dist - Regions
-    b_reg_buryatiya = 'Бурятия'
-    b_reg_prim_kray = 'Приморский край'
-    b_reg_habarovsk = 'Хабаровский край'
-    b_reg_amur = 'Амурская обл.'
-    b_reg_dal_vost_other = 'Прочие поиски по ДФО'
-    keyboard_dal_vost_reg_choice = [
-        [b_reg_buryatiya],
-        [b_reg_prim_kray],
-        [b_reg_habarovsk],
-        [b_reg_amur],
-        [b_reg_dal_vost_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Privolzhskiy Fed Dist - Regions
-    b_reg_bashkorkostan = 'Башкортостан'
-    b_reg_kirov = 'Кировская обл.'
-    b_reg_mariy_el = 'Марий Эл'
-    b_reg_mordovia = 'Мордовия'
-    b_reg_nizhniy = 'Нижегородская обл.'
-    b_reg_orenburg = 'Оренбургская обл.'
-    b_reg_penza = 'Пензенская обл.'
-    b_reg_perm = 'Пермский край'
-    b_reg_samara = 'Самарская обл.'
-    b_reg_saratov = 'Саратовская обл.'
-    b_reg_tatarstan = 'Татарстан'
-    b_reg_udmurtiya = 'Удмуртия'
-    b_reg_ulyanovsk = 'Ульяновская обл.'
-    b_reg_chuvashiya = 'Чувашия'
-    b_reg_privolz_other = 'Прочие поиски по ПФО'
-    keyboard_privolz_reg_choice = [
-        [b_reg_bashkorkostan],
-        [b_reg_kirov],
-        [b_reg_mariy_el],
-        [b_reg_mordovia],
-        [b_reg_nizhniy],
-        [b_reg_orenburg],
-        [b_reg_penza],
-        [b_reg_perm],
-        [b_reg_samara],
-        [b_reg_saratov],
-        [b_reg_tatarstan],
-        [b_reg_udmurtiya],
-        [b_reg_ulyanovsk],
-        [b_reg_chuvashiya],
-        [b_reg_privolz_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Severo-Kavkazskiy Fed Dist - Regions
-    b_reg_dagestan = 'Дагестан'
-    b_reg_stavropol = 'Ставропольский край'
-    b_reg_chechnya = 'Чечня'
-    b_reg_kabarda = 'Кабардино-Балкария'
-    b_reg_ingushetia = 'Ингушетия'
-    b_reg_sev_osetia = 'Северная Осетия'
-    b_reg_sev_kav_other = 'Прочие поиски по СКФО'
-    keyboard_sev_kav_reg_choice = [
-        [b_reg_dagestan],
-        [b_reg_stavropol],
-        [b_reg_chechnya],
-        [b_reg_kabarda],
-        [b_reg_ingushetia],
-        [b_reg_sev_osetia],
-        [b_reg_sev_kav_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Severo-Zapadniy Fed Dist - Regions
-    b_reg_vologda = 'Вологодская обл.'
-    b_reg_karelia = 'Карелия'
-    b_reg_komi = 'Коми'
-    b_reg_piter = 'Ленинградская обл.'
-    b_reg_murmansk = 'Мурманская обл.'
-    b_reg_pskov = 'Псковская обл.'
-    b_reg_archangelsk = 'Архангельская обл.'
-    b_reg_sev_zap_other = 'Прочие поиски по СЗФО'
-    keyboard_sev_zap_reg_choice = [
-        [b_reg_vologda],
-        [b_reg_komi],
-        [b_reg_karelia],
-        [b_reg_piter],
-        [b_reg_murmansk],
-        [b_reg_pskov],
-        [b_reg_archangelsk],
-        [b_reg_sev_zap_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Sibirskiy Fed Dist - Regions
-    b_reg_altay = 'Алтайский край'
-    b_reg_irkutsk = 'Иркутская обл.'
-    b_reg_kemerovo = 'Кемеровская обл.'
-    b_reg_krasnoyarsk = 'Красноярский край'
-    b_reg_novosib = 'Новосибирская обл.'
-    b_reg_omsk = 'Омская обл.'
-    b_reg_tomsk = 'Томская обл.'
-    b_reg_hakasiya = 'Хакасия'
-    b_reg_sibiria_reg_other = 'Прочие поиски по СФО'
-    keyboard_sibiria_reg_choice = [
-        [b_reg_altay],
-        [b_reg_irkutsk],
-        [b_reg_kemerovo],
-        [b_reg_krasnoyarsk],
-        [b_reg_novosib],
-        [b_reg_omsk],
-        [b_reg_tomsk],
-        [b_reg_hakasiya],
-        [b_reg_sibiria_reg_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Uralskiy Fed Dist - Regions
-    b_reg_ekat = 'Свердловская обл.'
-    b_reg_kurgan = 'Курганская обл.'
-    b_reg_tyumen = 'Тюменская обл.'
-    b_reg_hanty_mansi = 'Ханты-Мансийский АО'
-    b_reg_chelyabinks = 'Челябинская обл.'
-    b_reg_yamal = 'Ямало-Ненецкий АО'
-    b_reg_urals_reg_other = 'Прочие поиски по УФО'
-    keyboard_urals_reg_choice = [
-        [b_reg_ekat],
-        [b_reg_kurgan],
-        [b_reg_tyumen],
-        [b_reg_hanty_mansi],
-        [b_reg_chelyabinks],
-        [b_reg_yamal],
-        [b_reg_urals_reg_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Central Fed Dist - Regions
-    b_reg_belogorod = 'Белгородская обл.'
-    b_reg_bryansk = 'Брянская обл.'
-    b_reg_vladimir = 'Владимирская обл.'
-    b_reg_voronezh = 'Воронежская обл.'
-    b_reg_ivanovo = 'Ивановская обл.'
-    b_reg_kaluga = 'Калужская обл.'
-    b_reg_kostroma = 'Костромская обл.'
-    b_reg_kursk = 'Курская обл.'
-    b_reg_lipetsk = 'Липецкая обл.'
-    b_reg_msk_act = 'Москва и МО: Активные Поиски'
-    b_reg_msk_inf = 'Москва и МО: Инфо Поддержка'
-    b_reg_orel = 'Орловская обл.'
-    b_reg_ryazan = 'Рязанская обл.'
-    b_reg_smolensk = 'Смоленская обл.'
-    b_reg_tambov = 'Тамбовская обл.'
-    b_reg_tver = 'Тверская обл.'
-    b_reg_tula = 'Тульская обл.'
-    b_reg_yaroslavl = 'Ярославская обл.'
-    b_reg_central_reg_other = 'Прочие поиски по ЦФО'
-    keyboard_central_reg_choice = [
-        [b_reg_belogorod],
-        [b_reg_bryansk],
-        [b_reg_vladimir],
-        [b_reg_voronezh],
-        [b_reg_ivanovo],
-        [b_reg_kaluga],
-        [b_reg_kostroma],
-        [b_reg_kursk],
-        [b_reg_lipetsk],
-        [b_reg_msk_act],
-        [b_reg_msk_inf],
-        [b_reg_orel],
-        [b_reg_ryazan],
-        [b_reg_smolensk],
-        [b_reg_tambov],
-        [b_reg_tver],
-        [b_reg_tula],
-        [b_reg_yaroslavl],
-        [b_reg_central_reg_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Yuzhniy Fed Dist - Regions
-    b_reg_adygeya = 'Адыгея'
-    b_reg_astrahan = 'Астраханская обл.'
-    b_reg_volgograd = 'Волгоградская обл.'
-    b_reg_krasnodar = 'Краснодарский край'
-    b_reg_krym = 'Крым'
-    b_reg_rostov = 'Ростовская обл.'
-    b_reg_yuzhniy_reg_other = 'Прочие поиски по ЮФО'
-    keyboard_yuzhniy_reg_choice = [
-        [b_reg_adygeya],
-        [b_reg_astrahan],
-        [b_reg_volgograd],
-        [b_reg_krasnodar],
-        [b_reg_krym],
-        [b_reg_rostov],
-        [b_reg_yuzhniy_reg_other],
-        [b_fed_dist_pick_other],
-        [b_back_to_start],
-    ]
-
-    # Settings - Fed Dist - Regions
-    b_menu_set_region = 'настроить регион поисков'
-
-    full_list_of_regions = (
-        keyboard_dal_vost_reg_choice[:-1]
-        + keyboard_privolz_reg_choice[:-1]
-        + keyboard_sev_kav_reg_choice[:-1]
-        + keyboard_sev_zap_reg_choice[:-1]
-        + keyboard_sibiria_reg_choice[:-1]
-        + keyboard_urals_reg_choice[:-1]
-        + keyboard_central_reg_choice[:-1]
-        + keyboard_yuzhniy_reg_choice[:-1]
-        + [[b_fed_dist_other_r]]
-    )  # noqa – for strange pycharm indent warning
-    full_dict_of_regions = {word[0] for word in full_list_of_regions}
-
-    dict_of_fed_dist = {
-        b_fed_dist_dal_vos: keyboard_dal_vost_reg_choice,
-        b_fed_dist_privolz: keyboard_privolz_reg_choice,
-        b_fed_dist_sev_kaz: keyboard_sev_kav_reg_choice,
-        b_fed_dist_sev_zap: keyboard_sev_zap_reg_choice,
-        b_fed_dist_sibiria: keyboard_sibiria_reg_choice,
-        b_fed_dist_uralsky: keyboard_urals_reg_choice,
-        b_fed_dist_central: keyboard_central_reg_choice,
-        b_fed_dist_yuzhniy: keyboard_yuzhniy_reg_choice,
-    }
-
-    # Other menu
-    b_view_latest_searches = 'посмотреть последние поиски'
-    b_goto_community = 'написать разработчику бота'
-    b_goto_first_search = 'ознакомиться с информацией для новичка'
-    b_goto_photos = 'посмотреть красивые фото с поисков'
-    keyboard_other = [
-        [b_view_latest_searches],
-        [b_goto_first_search],
-        [b_goto_community],
-        [b_goto_photos],
-        [b_back_to_start],
-    ]
 
     # Admin - specially keep it for Admin, regular users unlikely will be interested in it
 
