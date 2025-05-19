2d4ecc9e30d59268a6532332c686be2cc47074e5
volodkindv <volodkindv@skbkontur.ru>
2025-05-19 10:44:29 +0500

    move tests

--- tests/dependencies/test_manage_topics.py
+++ tests/dependencies/test_manage_topics.py
@@ -1,38 +1,11 @@
-from datetime import datetime
 from random import randint
-from typing import NamedTuple
-from unittest.mock import MagicMock, patch
 
 import pytest
-from telegram import Bot
-from telegram.ext import ExtBot
 
 from _dependencies.topic_management import save_status_for_topic
-from manage_topics import main
-from tests.common import find_model, get_event_with_data
+from tests.common import find_model
 from tests.factories.db_factories import SearchFactory, get_session
 from tests.factories.db_models import ChangeLog, Search
-
-
-class Context(NamedTuple):
-    event_id: int
-
-
-def test_main():
-    event = get_event_with_data(
-        {
-            'topic_id': 123,
-            'visibility': True,
-            'status': 'on',
-        }
-    )
-
-    with (
-        patch.object(ExtBot, 'send_message'),
-        patch.object(Bot, 'get_me'),
-    ):
-        main.main(event, Context(event_id=123))
-        assert True
 
 
 @pytest.fixture
@@ -41,13 +14,11 @@ def topic_id() -> int:
 
 
 class TestSaveStatusForTopic:
-    def test_save_new_status(self, topic_id: int):
+    def test_save_new_status(self, topic_id: int, connection):
         SearchFactory.create_sync(search_forum_num=topic_id, status='old_status')
         new_status = 'new_status'
 
-        pool = main.sql_connect()
-        with pool.connect() as conn:
-            change_log_id = save_status_for_topic(conn, topic_id, new_status)
+        change_log_id = save_status_for_topic(connection, topic_id, new_status)
 
         assert find_model(get_session(), Search, search_forum_num=topic_id, status=new_status)
 
@@ -58,13 +29,11 @@ class TestSaveStatusForTopic:
         assert change_log.new_value == new_status
         assert change_log.change_type == 1
 
-    def test_save_existing_status(self, topic_id: int):
+    def test_save_existing_status(self, topic_id: int, connection):
         existing_status = 'existing_status'
         SearchFactory.create_sync(search_forum_num=topic_id, status=existing_status)
 
-        pool = main.sql_connect()
-        with pool.connect() as conn:
-            change_log_id = save_status_for_topic(conn, topic_id, existing_status)
+        change_log_id = save_status_for_topic(connection, topic_id, existing_status)
 
         assert change_log_id is None
 
@@ -73,13 +42,11 @@ class TestSaveStatusForTopic:
         change_log_count = get_session().query(ChangeLog).filter_by(search_forum_num=topic_id).count()
         assert change_log_count == 0
 
-    def test_save_status_for_nonexistent_topic(self, topic_id: int):
+    def test_save_status_for_nonexistent_topic(self, topic_id: int, connection):
         new_status = 'new_status'
         SearchFactory.create_sync(search_forum_num=topic_id)
 
-        pool = main.sql_connect()
-        with pool.connect() as conn:
-            change_log_id = save_status_for_topic(conn, topic_id, new_status)
+        change_log_id = save_status_for_topic(connection, topic_id, new_status)
 
         assert change_log_id is not None
 
