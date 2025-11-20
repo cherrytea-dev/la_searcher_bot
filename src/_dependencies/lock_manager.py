624e511f288f73dff1c1c32e4fbea6b3630f3de8
volodkindv <volodkindv@skbkontur.ru>
2025-11-20 08:21:03 +0500

    try to fix function lock

--- src/_dependencies/lock_manager.py
+++ src/_dependencies/lock_manager.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import contextmanager
 from typing import Iterator
 
@@ -13,19 +14,31 @@ class FunctionLockError(Exception):
 @contextmanager
 def lock_manager(conn: Connection, func_name: str, timeout_in_seconds: int) -> Iterator[None]:
     """Context manager to avoid situation when many instances running one function"""
+    timeout_in_seconds = 0  # TODO do we need it?
     fn_key = f'lock_function_{func_name}'
+
+    logging.info(f'Trying to lock function {func_name}')
     with conn.begin() as tr:
         try:
-            _set_session_timeout_for_transaction(conn, timeout_in_seconds)
+            _set_session_timeout_for_transaction(conn, 5)
             _create_record_for_function_if_not_exists(conn, fn_key)
             # TODO need separate table for locks, not key_value_storage
             _lock_record_in_transaction(conn, fn_key)
+            logging.info(f'Lock for function {func_name} ic acuqired')
 
             yield None
 
+            logging.info(f'Releasing lock for function {func_name}')
+
             tr.commit()
+            logging.info(f'Lock for function {func_name} is released')
+
         except (OperationalError, TimeoutError) as exc:
+            logging.info(f'Function {func_name} is locked by another process')
+
             tr.rollback()
+            logging.info(f'Lock for function {func_name}: exiting')
+
             raise FunctionLockError() from exc
 
 
