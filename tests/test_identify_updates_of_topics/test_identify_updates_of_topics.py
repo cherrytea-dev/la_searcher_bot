088048bf8c9f85acdcb7aae8deda7a8ff265d8b7
volodkindv <volodkindv@skbkontur.ru>
2026-06-11 19:21:29 +0500

    backup

--- tests/test_identify_updates_of_topics/test_identify_updates_of_topics.py
+++ tests/test_identify_updates_of_topics/test_identify_updates_of_topics.py
@@ -3,7 +3,10 @@ from unittest.mock import MagicMock, Mock, patch
 import pytest
 
 from identify_updates_of_topics import main
+from identify_updates_of_topics._utils.forum import ForumClient, ForumSearchItem
 from tests.common import get_event_with_data
+
+from .factories import ForumCommentItemFactory, ForumSearchItemFactory
 
 
 def test_main():
@@ -13,16 +16,26 @@ def test_main():
 
 
 def test_main_full_scenario(mock_http_get, patch_app_config):
+    """Just run without errors"""
+
     search_id = 11
     data = [search_id]
-    context = Mock()
-    context.event_id = 123
 
-    class FakeUpdater(main.SearchUpdater):
-        pass
+    class FakeForum(ForumClient):
+        def parse_search(self, search_num):
+            return ForumSearchItemFactory.build()
 
-    with patch.object(main, 'SearchUpdater', FakeUpdater):
-        main.main(get_event_with_data(data), context)
+        def get_raw_search_text(self, search_num):
+            return 'foo'
 
-    # what to check here?
-    pass
+        def get_comment_data(self, search_num, comment_num):
+            return ForumCommentItemFactory.build()
+
+        def get_replies_count(self, search_num):
+            return 1
+
+        def parse_coordinates_of_search(self, search_num):
+            return [0.0, 0.0, '', '']
+
+    with patch.object(main, 'ForumClient', FakeForum):
+        main.main(get_event_with_data(data), Mock())
