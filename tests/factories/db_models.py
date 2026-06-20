d531f981f70c55daa4404cf9d0e4e1db4dbbd972
volodkindv <volodkindv@skbkontur.ru>
2026-06-21 02:28:16 +0500

    fix warnings 2

--- tests/factories/db_models.py
+++ tests/factories/db_models.py
@@ -17,7 +17,7 @@ from sqlalchemy import (
     text,
 )
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
