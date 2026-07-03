6a739ba8e72cc5549042147b49fffe4ead6d8baf
Кот <kot@kot.bot>
2026-07-03 21:48:29 +0000

    chore: add hello world demo script

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Hello, LA Searcher Bot!"""
+
+
+def greet(name: str = 'LA Searcher Bot') -> str:
+    return f'Привет, {name}! Я Кот 🐱, и я здесь, чтобы помогать.'
+
+
+if __name__ == '__main__':
+    print(greet())
