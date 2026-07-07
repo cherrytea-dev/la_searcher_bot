c6308894fff3da27fdc00ed62e74ca1e3b3264ab
Кот <kot@kot.bot>
2026-07-07 20:45:20 +0000

    fix: update tests for DBClient refactor in api_get_active_searches

--- tests/test_api_get_active_searches.py
+++ tests/test_api_get_active_searches.py
@@ -1,7 +1,7 @@
 import datetime
 
 from polyfactory import Use
-from sqlalchemy.engine.base import Connection
+from sqlalchemy.engine.base import Engine
 
 from api_get_active_searches import main
 from tests.common import get_http_request
@@ -19,14 +19,14 @@ class ActiveSearchFactory(SearchFactory):
 
 
 class TestMain:
-    def test_main(self):
+    def test_main(self) -> None:
         request = get_http_request(method='POST', data={'app_id': 1})
 
         main.main(request)
 
         assert True
 
-    def test_main_empty_json(self):
+    def test_main_empty_json(self) -> None:
         request = get_http_request(method='POST', data={})
 
         resp = main.main(request)
@@ -35,7 +35,7 @@ class TestMain:
         assert 'app_id' in answer.reason
         assert 'validation error' in answer.reason
 
-    def test_main_no_json(self):
+    def test_main_no_json(self) -> None:
         request = {
             'body': 'not a json',
             'httpMethod': 'GET',
@@ -46,7 +46,7 @@ class TestMain:
         answer = main.FailResponse.model_validate_json(resp['body'])
         assert 'Invalid JSON' in answer.reason
 
-    def test_main_incorrect_app_id(self):
+    def test_main_incorrect_app_id(self) -> None:
         request = get_http_request(method='POST', data={'app_id': 'unexisting'})
 
         resp = main.main(request)
@@ -54,7 +54,7 @@ class TestMain:
         answer = main.FailResponse.model_validate_json(resp['body'])
         assert answer.reason == 'Incorrect app_id'
 
-    def test_main_cors(self):
+    def test_main_cors(self) -> None:
         request = get_http_request(method='OPTIONS')
 
         resp = main.main(request)
@@ -62,17 +62,18 @@ class TestMain:
         assert resp['statusCode'] == 204
 
 
-def test_get_searches_from_db(connection: Connection):
+def test_get_searches_from_db(connection_pool: Engine) -> None:
     folder1 = GeoFolderFactory.create_sync(folder_type='searches')
     search1 = ActiveSearchFactory.create_sync(forum_folder_id=folder1.folder_id)
     SearchHealthCheckFactory.create_sync(search_forum_num=search1.search_forum_num, status='ok')
     SearchFirstPostFactory.create_sync(search_id=search1.search_forum_num, actual=True)
 
-    searches = main.get_list_of_active_searches_from_db(connection, main.UserRequest(app_id=1))
+    db = main.DBClient(db=connection_pool)
+    searches = db.get_active_searches(main.UserRequest(app_id=1))
     assert searches  # TODO find correct params for db factory
 
 
-def test_get_query_results_with_folders(connection: Connection):
+def test_get_query_results_with_folders(connection_pool: Engine) -> None:
     folder1, folder2 = GeoFolderFactory.create_batch_sync(2, folder_type='searches')
 
     search1 = ActiveSearchFactory.create_sync(forum_folder_id=folder1.folder_id)
@@ -84,9 +85,10 @@ def test_get_query_results_with_folders(connection: Connection):
     SearchHealthCheckFactory.create_sync(search_forum_num=search2.search_forum_num, status='ok')
     SearchFirstPostFactory.create_sync(search_id=search2.search_forum_num, actual=True)
 
+    db = main.DBClient(db=connection_pool)
     depth_days = 30
-    folders_list = [folder1.folder_id, folder2.folder_id]
-    result = main.get_query_results(connection, depth_days, folders_list)
+    folders_list = [int(folder1.folder_id), int(folder2.folder_id)]
+    result = db._get_query_results(depth_days, folders_list)
 
     # Assert that the result is a list of Search objects with the correct data
     assert len(result) == 2
@@ -97,15 +99,16 @@ def test_get_query_results_with_folders(connection: Connection):
     assert result[1].forum_folder_id == search2.forum_folder_id
 
 
-def test_get_query_results_with_no_folders(connection: Connection):
+def test_get_query_results_with_no_folders(connection_pool: Engine) -> None:
     folder1 = GeoFolderFactory.create_sync(folder_type='searches')
     search1 = ActiveSearchFactory.create_sync(forum_folder_id=folder1.folder_id)
     SearchHealthCheckFactory.create_sync(search_forum_num=search1.search_forum_num, status='ok')
     SearchFirstPostFactory.create_sync(search_id=search1.search_forum_num, actual=True)
 
+    db = main.DBClient(db=connection_pool)
     depth_days = 30
-    folders_list = []
-    result = main.get_query_results(connection, depth_days, folders_list)
+    folders_list: list[int] = []
+    result = db._get_query_results(depth_days, folders_list)
 
     assert result
     assert isinstance(result[0], main.Search)
