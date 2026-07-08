16e22fb6aec58dd81824a65b36fcd37fdb24937b
Кот <kot@kot.bot>
2026-07-08 21:04:52 +0000

    fix: remove ReplyKeyboardMarkup from search_follow_mode callback response
    После inline callback бот отправлял новое сообщение с ReplyKeyboardMarkup,
    что вызывало MESSAGE_TOO_LONG от Telegram API (баг воспроизводился на
    включении режима отслеживания поисков).
    
    Клавиатура уже видна пользователю до callback, повторно показывать её
    не нужно. Убираем reply_markup из ответа.

--- src/communicate/_utils/handlers/callback_handlers.py
+++ src/communicate/_utils/handlers/callback_handlers.py
@@ -4,7 +4,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from _dependencies.common.commons import SearchFollowingMode
 
-from ..buttons import TopicTypeInlineKeyboardBuilder, reply_markup_main
+from ..buttons import TopicTypeInlineKeyboardBuilder
 from ..common import (
     NOT_FOLLOWING_MARK,
     InlineButtonCallbackData,
@@ -128,7 +128,7 @@ def handle_search_follow_mode(ctx: TGHandlerContext) -> None:
         bot_message = 'Все пометки отслеживания поисков сброшены. Переоткройте список активных поисков.'
 
     ctx.answer_callback(bot_message)
-    ctx.reply(text=bot_message, reply_markup=reply_markup_main)
+    ctx.reply(text=bot_message)
 
 
 @tg_handle(callback_data='search_follow_mode')
